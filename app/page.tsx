da56c4b432d694fa27912e38cd78721a44c713b2
zelemm <sunil@srsss.in>
2024-03-01 22:38:46 +0530

    Pushing new adjustment for home page

--- app/page.tsx
+++ app/page.tsx
@@ -8,17 +8,13 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-20">
         <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+        <div className="flex flex-col  gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
+            <strong>Welcome to SRS Software Systems - Empowering Businesses through Innovative Solutions Since August 2010.</strong> 
           </p>
           <Link
             href="/login"
@@ -27,21 +23,78 @@ export default function Page() {
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          <Image
-            src="/hero-desktop.png"
-            width={1000}
-            height={760}
-            className='hidden md:block'
-            alt='Screenshot of the dashboard project showing desktop version'
-          />
-          <Image
-            src="/hero-mobile.png"
-            width={560}
-            height={620}
-            className='block md:hidden'
-            alt='Screenshot of the dashboard project showing desktop version'
-          />
+        <div className="fflex flex-col  gap-6 rounded-lg bg-gray-50 px-6 py-10 ">
+          <div>
+          At SRS Software Systems, we take pride in our journey as a pioneering software company, delivering cutting-edge solutions to businesses worldwide. 
+          With a rich experience spanning over a decade, we specialize in providing comprehensive software development services, leveraging the latest technologies and frameworks.
+          </div>
+          <h2 className='pt-2 pb-2 text-xl font-medium'>Our Expertise:</h2>
+
+          <ol>
+          <li>
+            <h2 className='pt-2 pb-2 text-md font-medium'>Web Development:</h2>
+              <ul>
+              <li>PHP</li>
+              <li>Python</li>
+              <li>Laravel</li>
+              <li>Django</li>
+              <li>GoLang</li>
+              <li>Next.js</li>
+              </ul>
+            </li>
+            <li>
+              <h2 className='pt-2 pb-2 text-md font-medium'>CRM/ERP/Accounting Solutions:</h2>
+              <ul>
+              <li>Zoho CRM</li>
+              <li>Xero API</li>
+              <li>Infusionsoft</li>
+              <li>Odoo</li>
+              <li>Tradegecko</li>
+              <li>Cliniko</li>
+              <li>Quickbooks</li>
+              <li>Custom CRM/ERP Solution</li>
+              </ul>
+            </li>
+            <li>
+              <h2 className='pt-2 pb-2 text-md font-medium'>API Integration:</h2>
+              <ul>
+              <li>Seamless integration of diverse APIs to enhance functionality and connectivity.</li>
+              <li>Google API</li>
+              <li>Cloud Server Setup and Maintenance</li>
+              </ul>
+            </li>
+            <li>
+              <h2 className='pt-2 pb-2 text-md font-medium'>Database Management:</h2>
+              <ul>
+              <li>MySQL</li>
+              <li>PostgreSQL</li>
+              <li>mongoDB</li>
+              </ul>
+            </li>
+          </ol>
+          <h2 className='pt-2 pb-2 text-xl font-medium'>Our Commitment:</h2>
+          <ul>
+          <li>
+            <strong>Innovation:</strong> We thrive on pushing the boundaries of technology to deliver innovative solutions that cater to the dynamic needs of businesses.
+            </li>
+            <li>
+            <strong>Reliability:</strong> With a decade-long presence in the market, we have established ourselves as a reliable partner for businesses seeking robust and scalable software solutions.
+            </li>
+            <li>
+            <strong>Customization:</strong> Recognizing that each business is unique, we craft tailor-made solutions to meet specific requirements, ensuring optimal performance and user satisfaction.
+            </li>
+            <li>
+            <strong>Customer-Centric Approach:</strong> Our focus is on understanding and addressing the unique challenges faced by our clients. We believe in building enduring relationships through exceptional service.
+            </li>
+            <li>
+            <strong>Transparent Communication:</strong> We believe in transparent and open communication, keeping our clients informed at every stage of the development process.
+            </li>
+          </ul>
+          
+          Whether you are looking to revamp your online presence, streamline business processes, or implement advanced CRM solutions, <strong>SRS Software Systems</strong> is your go-to partner. Join hands with us to embark on a journey of digital transformation and elevate your business to new heights.
+
+          <p className='pt-5'>Write us at <Link href="mail:sunil@srsss.in" className='text-blue-800 rounded-md p-3 text-sm font-medium hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3'>sunil@srsss.in</Link> 
+          today to discuss how we can transform your ideas into reality and drive your business towards success.</p>
         </div>
       </div>
     </main>
--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,13 +1,21 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
 
 export default function AcmeLogo() {
   return (
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      {/* <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" /> */}
+      <Image
+        src="/srs_logo_95.jpg"
+        width={60}
+        height={100}
+        alt='Company Logo'
+        className=''
+      />
+      <p className="pl-4 text-[34px]">SRS Software Systems</p>
     </div>
   );
 }
