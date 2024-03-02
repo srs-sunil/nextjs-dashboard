3c7874f633139eebaa83fa6b984fc203440e9848
zelemm <sunil@srsss.in>
2024-03-02 14:48:11 +0530

    Removing Login Link for now

--- app/page.tsx
+++ app/page.tsx
@@ -16,12 +16,12 @@ export default function Page() {
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to SRS Software Systems - Empowering Businesses through Innovative Solutions Since August 2010.</strong> 
           </p>
-          <Link
+          {/* <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-          </Link>
+          </Link> */}
           <Image 
             src="/Sunil_Passport_Size_Photo.png"
             width={400}
