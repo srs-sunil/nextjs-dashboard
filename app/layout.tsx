e7907a445da0211aae21edbcd8f22a8e30159149
zelemm <sunil@srsss.in>
2024-03-30 17:59:44 +0530

    Pushing changes to Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/app/ui/global.css'
 import { inter } from '@/app/ui/fonts';
+import Script from 'next/script'
 
 export default function RootLayout({
   children,
@@ -8,6 +9,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=UA-54318939-1"
+      />
+
+      <Script id="google-analytics">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', ${'${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}'});
+        `}
+      </Script>
+      </head>
       <body className={ `${inter.className} antialiased` }>{children}</body>
     </html>
   );
